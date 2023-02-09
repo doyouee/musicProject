32eb941cbb2d7f96e0fec3704e21c8b6cb63764c
doyouee <aquariu92s@naver.com>
2023-02-09 23:12:14 +0900

    230209_artistGroup 기능 적용

--- js/artistgroup.js
+++ js/artistgroup.js
@@ -1,4 +1,10 @@
 //artistgroup.js
+
+//230209 밖에다가 global 변수로 두고 써야 편함
+let companyPage = 0
+let keyword = ""
+
+
 document.addEventListener("DOMContentLoaded", function(){
     elemId("imgfile").addEventListener("change", function(e) {
         if(e.target.files && e.target.files[0]) { // 파일이 있다면
@@ -27,4 +33,68 @@ document.addEventListener("DOMContentLoaded", function(){
             console.log(err)
         })
     })
+
+    // 230209
+    elemId("company_sel").addEventListener("click",function(){
+        getCompanyList();
+    })
 })
+
+
+// 230209 html 아니고 ajax 쓰면 파라미터 구체화 가능
+function getCompanyList(page, keyword) {
+    let url = baseURL + "/company/list"
+    let params = {}
+    if(page != undefined && page != null) {
+        // url += "?page=" + page -> 파라미터 구체화 안되는 방식
+        params.page = page;
+    }
+    if(keyword != undefined && keyword != null && keyword != "") {
+        // url += "&keyword=" + keyword -> 파라미터 구체화 안되는 방식
+        params.keyword = keyword
+    }
+    console.log(params)
+    axios.get(url, {params:params}).then(function(result) {
+        // console.log(result)
+        const list = result.data.list;
+        let tbodyContent = ""
+        list.forEach((item) => {
+            tbodyContent +=   
+                    // 선생님 방법
+                    // '<tr>'+
+                    //     '<td> + item.pubSeq + </td>'+
+                    //     '<td> + item.pubName + </td>'+
+                    //     '<td>'+
+                    //         '<button onclick="selectCompany(' + item.pubSeq + ', \'' + item.pubName + '\')">선택</button>'+
+                    //     '</td>'+
+                    // '</tr>'
+
+                    //영준님 방법(백틱)
+                    `<tr>
+                        <td>${item.pubSeq}</td>
+                        <td>${item.pubName}</td>
+                        <td>
+                            <button onclick="selectCompany(${item.pubSeq}, '${item.pubName}')">선택</button>
+                        </td>
+                    </tr>`
+        })
+        let pagers = ""
+        for(let i=0; i<result.data.totalPage; i++) {
+            // pagers += '<button onclick="getCompanyList(' + i + '. \'' + elemId("search_keyword").value + '\')">' + (i+1) + '</button>'
+            // 위 코드를 백틱을 사용하면 (백틱의 단점 : JS 사용법과 겹쳐서 헷갈릴 가능성 있음)
+            pagers += `<button onclick="getCompanyList(${i}, '${elemId("search_keyword").value}')">${i+1}</button>`
+        }
+        document.querySelector(".company_popup tbody").innerHTML = tbodyContent
+        document.querySelector(".popup_pager_area").innerHTML = pagers
+    })
+    // 서치버튼이 클릭됐을때 이벤트 추가
+    elemId("search_btn").addEventListener("click", function() {
+        getCompanyList(0, elemId("search_keyword").value)
+    })
+}
+//getCompanyList()
+
+function selectCompany(seq, name) {
+    elemId("company").value = seq
+    elemId("company_name").innerHTML = name
+}
